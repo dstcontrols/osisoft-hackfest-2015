be1a1904e9accc7c1546e5498f459ef884a9afc5
adkenyon <akenyon@DST-L5075.DSTControls.local>
2015-02-26 10:46:32 -0800

    data simulator project created

---
+++ HackfestDataSimulator/HackfestDataSimulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.Qi;
+
+namespace HackfestDataSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
