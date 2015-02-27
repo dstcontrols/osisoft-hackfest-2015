d16d29d9baee5c8df8fbca647b7d67cd2211914e
Lonnie Bowling <LBowling@DST-L5136.DSTControls.local>
2015-02-27 14:09:32 -0800

    More gages and binding.

--- QiOEE/QiOEE/Controllers/OEEController.cs
+++ QiOEE/QiOEE/Controllers/OEEController.cs
@@ -1,4 +1,5 @@
 ﻿using QiOEE.Services;
+using QiQuery;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,26 @@ namespace QiOEE.Controllers
 {
     public class OEEController : ApiController
     {
+        public static uint Tenant = 10000;
+        public static string Endpoint = "http://historiandev.cloudapp.net:3380";
+        public static string TagPrefix = "P6-8";
+
+        public QiQuery.Client QiData { get; set; }
+        public OEEController()
+        {
+            QiData = new Client(Endpoint,Tenant,TagPrefix);
+            
+        }
         // GET: api/OEE
-        public IEnumerable<int> Get()
+        public OEEModel Get()
         {
             int[] data = { 1, 2, 3, 4,6,7,8,9,10,11,6,3 };
-
-            return data;// QiService.DataTest();
+            var testData = new QiQuery.OEEModel(DateTime.Now.AddHours(-5), DateTime.Now.AddHours(-1));
+            testData.Availability = 60.50;
+            testData.Throughput = 94.34;
+            testData.Quality = 91.35;
+            testData.OEE = testData.Availability / 100 * testData.Throughput / 100 * testData.Quality / 100 * 100;
+            return testData;// QiData.GetOEE(DateTime.Now.AddHours(-5), DateTime.Now.AddHours(-1), 0);// QiService.DataTest();
         }
 
         // GET: api/OEE/5
