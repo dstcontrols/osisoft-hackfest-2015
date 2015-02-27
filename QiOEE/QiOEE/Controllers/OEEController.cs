4d5a36bd5e6b56b2da2514a4a8c0ef7931c0899b
Lonnie Bowling <LBowling@DST-L5136.DSTControls.local>
2015-02-26 16:15:04 -0800

    Added QiOEE

---
+++ QiOEE/QiOEE/Controllers/OEEController.cs
@@ -0,0 +1,40 @@
+﻿using QiOEE.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QiOEE.Controllers
+{
+    public class OEEController : ApiController
+    {
+        // GET: api/OEE
+        public string Get()
+        {
+            return QiService.DataTest();
+        }
+
+        // GET: api/OEE/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/OEE
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/OEE/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/OEE/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ QiOEE/QiOEE/Services/QiService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace QiOEE.Services
+{
+    public static class QiService
+    {
+        static public string DataTest()
+        {
+
+            string url = " http://historiandev.cloudapp.net:3380/Qi/Streams/p6-1.scheduled_availability/Data/GetWindowValues?startIndex=2-25-2015&endIndex=2-27-2015";
+            HttpWebRequest authRequest = (HttpWebRequest)WebRequest.Create(url);
+            authRequest.Method = "GET";
+            authRequest.ContentType = "application/json";
+            authRequest.Headers["QiTenant"] = "10000";
+
+            try
+            {
+                var response = (HttpWebResponse)authRequest.GetResponse();
+
+                string result = "null";
+
+                using (Stream stream = response.GetResponseStream())
+                {
+
+                    StreamReader sr = new StreamReader(stream);
+                    result = sr.ReadToEnd();
+                    sr.Close();
+
+                }
+                return result;
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                System.Diagnostics.Debug.WriteLine(e.Message + "\n" + e.ToString());
+                throw (e);
+            }
+        }
+    }
+
+}
